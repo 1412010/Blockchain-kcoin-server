26fcf9a447e868d1e8c5f8b2970a6f90de9c565f
Tam <thanhtam230496@gmail.com>
2018-01-01 22:22:31 +0700

    Cập nhật cấu trúc transaction model

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var transactionModelSchema = new mongoose.Schema({
     _hash: {type:String, required: true},
     _confirmCode: {type: String, required: true},
-    _state: {type: String, required: true}
+    _state: {type: String, required: true},
+    _dateInit: {type: Date, required: true}, //thời gian khởi tạo giao dịch
+    _dateSuccess: {type: Date} //thời gian giao dịch được xác nhận
 });
 mongoose.model('transactionModel', transactionModelSchema);
 
