37462dcbfbe547ebd64bd63c2e7b666535aaafcb
Tam <thanhtam230496@gmail.com>
2018-01-01 11:33:14 +0700

    Thêm chức năng xác nhận từ mã xác nhận, đăng nhập và các module liên quan

--- controllers/api/restApi.js
+++ controllers/api/restApi.js
@@ -2,18 +2,21 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 var accountModel = require('../../models/accountModel');
+var confirmAccountCode = require('../../models/confirmAccountCode');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 var randomstring = require("randomstring");
 var nodemailer = require('nodemailer');
 var smtpTransport = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'myauctionwebapp@gmail.com',
-        pass: 'tamhieuga'
-    }
+	service: 'Gmail',
+	auth: {
+		user: 'myauctionwebapp@gmail.com',
+		pass: 'tamhieuga'
+	}
 });
+const secretKey = 'key';
 /* PUT students listing. */
-router.put('/Register', function(req, res, next) {
+router.put('/Register', function (req, res, next) {
 	var ePass = crypto.createHash('md5').update(req.body.password).digest('hex');
 
 	var condition = {
@@ -28,8 +31,8 @@ router.put('/Register', function(req, res, next) {
 		}
 
 		if (row.length == 0) {
-			request('https://api.kcoin.club/generate-address', function (error, response, body){
-				if(error){
+			request('https://api.kcoin.club/generate-address', function (error, response, body) {
+				if (error) {
 					console.log(error);
 					return read.status(500).send("Không thể đăng ký");
 				}
@@ -55,6 +58,7 @@ router.put('/Register', function(req, res, next) {
 						return read.status(500).send("Không thể đăng ký");
 					} else {
 						console.log(row);
+						//gửi mã xác nhận
 						var text = "Mã xác nhận của tài khoản bạn là: " + confirmCode;
 						var mailOptions = {
 							from: "Web Auction <myauctionwebapp@gmail.com>", // sender address
@@ -63,12 +67,26 @@ router.put('/Register', function(req, res, next) {
 							text: text // plaintext body
 						};
 						console.log(mailOptions);
-		
-						smtpTransport.sendMail(mailOptions, function(error, response) {
+
+						smtpTransport.sendMail(mailOptions, function (error, response) {
 							if (error) {
 								console.log(error);
 							} else {
 								console.log("Message sent: " + response.message);
+							}
+						});
+						//gửi mã xác nhận
+						var dateConfirmCode = {
+							_confirmCode: confirmCode,
+							_idAccount: row.id
+						}
+						//thêm mã xác nhận ứng với tài khoản
+						confirmAccountCode.create(dateConfirmCode, function (error, row1) {
+							if (error) {
+								console.log(error);
+								return read.status(500).send("Không thể đăng ký");
+							} else {
+								console.log(row1);
 							}
 						});
 
@@ -76,13 +94,79 @@ router.put('/Register', function(req, res, next) {
 					}
 				});
 			});
-			
-			
+
+
 		} else {
 			return res.status(409).send("Email đã tồn tại!")
 		}
 	});
 });
 
+router.post('/ConfirmAccount', function (req, res, next) {
+	var condition = {
+		_confirmCode: req.body.code
+	};
+	confirmAccountCode.find(condition, function (error, row) {
+		if (error) {
+			return res.status(400).send("Xác nhận thất bại!")
+		}
+		if (row.length > 0) {
+			var id = row[0]._idAccount;
+			var condition2 = {
+				_id: id
+			}
+			var newData = {
+				_isActive: true
+			}
+			accountModel.findOneAndUpdate(condition2, newData, { new: true }, function (error, row1) {
+				if (error) {
+					return res.status(400).send("Xác nhận thất bại!")
+				} else {
+					var mess = {
+						message: "Xác nhận tài khoản thành công",
+						address: row1._address
+					}
+					return res.json(mess);
+				}
+			});
+		}
+	});
+});
 
+router.post('/Login', function (req, res, next) {
+	var ePass = crypto.createHash('md5').update(req.body.password).digest('hex');
+	var condition = {
+		_email: req.body.email,
+		_password: ePass
+	};
+	accountModel.find(condition, function (error, row) {
+		if (error) {
+			console.log(error);
+			return res.status(500).send("Đăng nhập thất bại!");
+		}
+
+		if (row.length == 0) {
+			return res.status(400).send("Email hoặc Password không đúng");
+		} else {
+			if (row[0]._isActive) {
+				var payload = {
+					_email: row[0]._email,
+					_address: row[0]._address,
+					_role: row[0]._role,
+					_realBalance: row[0]._realBalance,
+					_availableBalance: row[0]._availableBalance
+				};
+				var token = jwt.sign(payload, secretKey);
+				var result = {
+					data: payload,
+					token: token
+				};
+				console.log(result);
+				return res.json(result);
+			} else {
+				return res.status(403).send("Tài khoản chưa kích hoạt!");
+			}
+		}
+	});
+})
 module.exports = router;
