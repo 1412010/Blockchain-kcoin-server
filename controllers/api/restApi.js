e95f382804ce22897df4f291924454e0dc31e5a8
LVHieu8396 <le.hieu.lvh@gmail.com>
2018-01-01 21:36:51 +0700

    cập nhật get /Address

--- controllers/api/restApi.js
+++ controllers/api/restApi.js
@@ -106,7 +106,7 @@ router.put('/Register', function (req, res, next) {
 
 
 //Lấy thông tin tài khoản
-router.get('/address', function(req, res) {
+router.get('/Address', function(req, res) {
 	accountModel.find(req.params.address, function(error, account) {
 		if (error) {
 			console.log(error);
@@ -119,7 +119,10 @@ router.get('/address', function(req, res) {
 				address: account._address,
 				email: account._email
 			}
+			console.log(data);
+			return res.json(data);
 		}
+		return res.status(500).send("Xảy ra lỗi khi lấy thông tin tài khoản");
 	})
 });
 
