5d323e9e6916b0867b741333d1fd9785d24e03d0
Tam <thanhtam230496@gmail.com>
2018-01-02 15:03:48 +0700

    Cập nhật inputs và outputs model

--- models/inputsModel.js
+++ models/inputsModel.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var inputsModelSchema = new mongoose.Schema({
-    _idTrans: {type: String, required: true},//id ánh xạ đến bảng transaction
     _hash: {type:String, required: true},
     _referencedOutputHash: {type: String, required: true},//hash giao dịch chứa output người gửi
     _referencedOutputIndex: {type: Number, required: true}
--- models/outputsModel.js
+++ models/outputsModel.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var outputsModelSchema = new mongoose.Schema({
-    _idTrans: {type: String, required: true}//id ánh xạ đến bảng transaction
     _hash: {type:String, required: true},
     _output: {type: String, required: true},//địa chỉ lấy từ lock script
     _index: {type: Number, required: true},
