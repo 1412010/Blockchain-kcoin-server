ff50c5979e64b2a408e27c62064e3e9fbf468b3d
Tam <thanhtam230496@gmail.com>
2018-01-02 14:30:43 +0700

    Cập các model: transaction, inputs, outputs. Thêm xử lý ban đầu cho api  rút tiền

--- controllers/api/restApi.js
+++ controllers/api/restApi.js
@@ -17,6 +17,15 @@ var smtpTransport = nodemailer.createTransport({
 const secretKey = 'key';
 var utils = require('../../fn/utils');
 var transactions = require('../../fn/transactions');
+const KHOITAO = 'KHỞI TẠO';
+const DANGXULY = 'ĐANG XỬ LÝ';
+const HOANTHANH = 'HOÀN THÀNH';
+
+//hash nhận tiền từ thầy: d6fd4a290c22190d6c414f51c96a7eb800c1705e83d3931861f562935c1f831c
+//outputeindex: 1
+//"address":"a32426e59e7a91d1fd90fdbf1b30df20c60756cbbfb8cdc1d21f9131dc674565"
+//"privateKey":"2d2d2d2d2d424547494e205253412050524956415445204b45592d2d2d2d2d0a4d4949435777494241414b42675144696b39666b64427254534a3264615345493848484437306d4b495777666a6f6444367a5668472f3534715265684c7145620a4277343764583475706d7a694a54622f676d746a5465374f52622f79644a4e55357a755a393055482b416b4430535346424235346c6a5878344252737543454e0a624e444959574258487742574e6f6143434858623669506769744f4e73544c7a31717947474336725363696a7448536a4b643636347064736b514944415141420a416f4741645a727070364941506f546c6b7376376f716b7a55394644615a673569424f783844594a43664f2b33534266724945496a482f59436c4c4e415366500a516e36776b6d552b6d6c666353574c343437395172665173415a2b4f435276346737376a762b3865744659425644684159526233634b7237544957664d6833320a32617053326b7548723830365554536f3545337355475a50326635774e4b4951626f2f5970574e52663155564d325543515144376d6e54794e644442707776390a7179695071707053437433446b6b69784832314d78456c5075745237452f4e62497a72754433565857535a483845793957692f47536a377431695a69624164670a745568636467686e416b4541356f6c764570733776417241796d2b6b364e6655765669357867312b346d75486267513044374c6e4174626b5a43787738766b4c0a6365556e306949574f62653462666e3731576b6b534573374f58586a5644306f52774a416559724d306244546a716b4f7274542b2f422f792b6f304c516732700a6c663754677845545254504172314163384472646e65445430345661613867576d695932432f6b54327a726e51675a6a726745453272326549774a41505764390a336b6261736b7471376f437550426241343234756a5549712b63514b58704e67436f357357615644554374474b6e474c5374727573466e4438627574744231520a70705a4b773762474835663235516b6c49514a414c2f4e51394466547a652f794577456c6372486f2f626d334831676543444c2f5a69796751794a696a6455310a736f48436f574c77454f4d697177564a596934794d7a4154585975467035354747544844395942544c673d3d0a2d2d2d2d2d454e44205253412050524956415445204b45592d2d2d2d2d0a"
+//"publicKey":"2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d4947664d413047435371475349623344514542415155414134474e4144434269514b42675144696b39666b64427254534a3264615345493848484437306d4b0a495777666a6f6444367a5668472f3534715265684c7145624277343764583475706d7a694a54622f676d746a5465374f52622f79644a4e55357a755a393055480a2b416b4430535346424235346c6a5878344252737543454e624e444959574258487742574e6f6143434858623669506769744f4e73544c7a31717947474336720a5363696a7448536a4b643636347064736b514944415141420a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a"
 /* PUT students listing. */
 router.put('/Register', function (req, res, next) {
 	var ePass = crypto.createHash('md5').update(req.body.password).digest('hex');
@@ -168,6 +177,73 @@ router.post('/Login', function (req, res, next) {
 			} else {
 				return res.status(403).send("Tài khoản chưa kích hoạt!");
 			}
+		}
+	});
+})
+
+router.post('/Transaction', function (req, res, next) {
+	var inputAddress = req.body.inputAddress;//tài khoản yêu cầu gửi
+	var outputAddress = req.body.outputAddress;//địa chỉ nhận
+	var value = req.body.value;
+	var condition = {
+		_address: inputAddress
+	}
+	accountModel.find(condition, function (error, row) {
+		if (error) {
+			return res.status(400).send("Thêm giao dịch thất bại")
+		}
+		if (row.length > 0) {
+			console.log(row);
+			const key = {
+				address: row[0]._address,
+				privateKey: row[0]._privateKey,
+				publicKey: row[0]._publicKey
+			}
+
+			// Generate transacitons
+			let bountyTransaction = {
+				version: 1,
+				inputs: [],
+				outputs: []
+			};
+			//Tìm referenceOutputsHashes hợp lệ
+			let keys = [];
+			// referenceOutputsHashes.forEach(hash => {
+			// 	bountyTransaction.inputs.push({
+			// 	  referencedOutputHash: hash,
+			// 	  referencedOutputIndex: 0,
+			// 	  unlockScript: ''
+			// 	});
+			// 	keys.push(key);
+			//   });
+
+			bountyTransaction.inputs.push({
+				referencedOutputHash: "d6fd4a290c22190d6c414f51c96a7eb800c1705e83d3931861f562935c1f831c",
+				referencedOutputIndex: 1,
+				unlockScript: ''
+			});
+			keys.push(key);
+
+			// Change because reference output must be use all value
+			const change = 9900;
+			bountyTransaction.outputs.push({
+				value: change,
+				lockScript: 'ADD ' + 'a32426e59e7a91d1fd90fdbf1b30df20c60756cbbfb8cdc1d21f9131dc674565'
+			});
+
+			//Output to all destination 
+			bountyTransaction.outputs.push({
+				value: value,
+				lockScript: 'ADD ' + outputAddress
+			});
+			console.log(bountyTransaction);
+			// Sign
+			transactions.sign(bountyTransaction, keys);
+			console.log(bountyTransaction);
+			return res.json(bountyTransaction);
+
+		} else {
+			return res.status(400).send("Địa chỉ gửi không đúng!")
 		}
 	});
 })
--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 var transactionModelSchema = new mongoose.Schema({
-    _hash: {type:String, required: true},
+    _hash: {type:String},
+    _inputAddress: {type: String, required: true}, //địa chỉ gửi
+    _outputAddress: {type: String, required: true}, //địa chỉ nhận
+    _value: {type: Number, required: true}, //số tiền gửi
     _confirmCode: {type: String, required: true},
     _state: {type: String, required: true},
     _dateInit: {type: Date, required: true}, //thời gian khởi tạo giao dịch
